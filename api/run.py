65481c884df1cae317e5389222221bd503c19465
Sina Moghtader Far <Moghtaderfar@gmail.com>
2026-02-03 11:53:12 +0100

    start fetching data from backend as JSON

--- api/run.py
+++ api/run.py
@@ -1,10 +1,20 @@
 # run.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.controllers.energy_controller import router as energy_router
 
 app = FastAPI(title="Germany Energy Forecast API")
 
-# Include controller routes
+# CORS config (for React)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Include API routes
 app.include_router(energy_router)
 
 if __name__ == "__main__":
